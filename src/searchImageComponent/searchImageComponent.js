6985280626d02501ee71a4786e0c8759aa808758
ZOLTAN BORBIRO <zazib03@gmail.com>
2021-06-07 16:13:30 +0200

    1. feladat kész

--- src/searchImageComponent/searchImageComponent.js
+++ src/searchImageComponent/searchImageComponent.js
@@ -23,9 +23,9 @@ class SearchImage extends ContentComponent {
     dogbreed = dogbreed.split(' ');
     // a dogbreed változó innentől egy tömb
     if (dogbreed.length === 1) {
-      urlString = `https://dog.ceo/api/breed/${dogbreed[0]}/images`;
+      urlString = `https://dog.ceo/api/breed/${dogbreed[0].toLowerCase()}/images`;
     } else if (dogbreed.length === 2) {
-      urlString = `https://dog.ceo/api/breed/${dogbreed[1]}/${dogbreed[0]}/images`;
+      urlString = `https://dog.ceo/api/breed/${dogbreed[1].toLowerCase()}/${dogbreed[0].toLowerCase()}/images`;
     }
     const response = await fetch(urlString);
     const data = await response.json();
